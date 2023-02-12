410596cdae71b78984b2fca84e2125b3dc0140ba
shu-william <williamshu@outlook.com>
2023-02-12 00:47:27 -0800

    updated app and component files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard 
+      firstName = { "Gary" } 
+      lastName = { "Snail" } 
+      age = { 20 } 
+      hairColor = { "Blue" } />
+      <PersonCard 
+      firstName = { "Wilbur" } 
+      lastName = { "Wilburliams" } 
+      age = { 26 } 
+      hairColor = { "Black" } />
+      <PersonCard 
+      firstName = { "Silver" } 
+      lastName = { "Goldilocks" } 
+      age = { 23 } 
+      hairColor = { "Blonde" } />
+      <PersonCard 
+      firstName = { "Arthur" } 
+      lastName = { "Paul-Blarther" } 
+      age = { 44 } 
+      hairColor = { "Brown" } />
     </div>
   );
 }
