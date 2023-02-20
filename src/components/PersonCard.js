57f4535a7208a10a0b9813941509bf42ea7fc2a7
shu-william <williamshu@outlook.com>
2023-02-19 18:57:59 -0800

    updated useState functionality

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PersonCard = (props) => {
-    const { firstName, lastName, age, hairColor} = props;
+    const { firstName, lastName, hairColor} = props;
+    const [ age, setAge ] = useState(props.age);
     return (
         <div>
             <h2>{ lastName }, { firstName }</h2>
             <p>Age: { age }</p>
             <p>Hair Color: { hairColor }</p>
+            <button onClick={ () => setAge(age + 1) }>Birthday Button for { firstName }</button>
         </div>
     );
 }
